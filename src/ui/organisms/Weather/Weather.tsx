7441a5637b9a3c7d21ff5eed8db7950ae71af0a6
Theodore Uzun <53522791+Theo-projects@users.noreply.github.com>
2020-08-05 14:35:19 +0300

    fix-v11

--- src/ui/organisms/Weather/Weather.tsx
+++ src/ui/organisms/Weather/Weather.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import { DailyObj } from 'ui/molecules/DailyObj/DailyObj';
 import { HourlyObj } from 'ui/molecules/HourlyObj/HourlyObj';
